f30e2ca3447cd0c8b956eecd24e628325de38900
wandrad3 <wesleyandrade298@gmail.com>
2021-08-18 20:59:54 -0300

    first rest api testing deploy heroku

---
+++ src/test/java/com/digital/innovation/one/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.innovation.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
